24efbbf4aa491d4ba272f1f9123f0c8f2f914aaf
vss84 <vssorroche@gmail.com>
2024-05-12 15:42:38 -0300

    Move headers.

--- Aliases.h
+++ Aliases.h
@@ -1,15 +1,8 @@
 #pragma once
 
 #include <vector>
-#include <limits>
-
 
 using Price = std::int32_t;
 using Quantity = std::uint32_t;
 using OrderId = std::uint64_t;
 using OrderIds = std::vector<OrderId>;
-
-struct Constants
-{
-	static const Price InvalidPrice = std::numeric_limits<Price>::quiet_NaN();
-};
